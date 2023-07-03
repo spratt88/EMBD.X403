c20ae8f5ff195b40f3a33c2dac043eb5c231f40f
spratt <spratt@ubuntu.myguest.virtualbox.org>
2023-07-03 13:32:23 -0400

    taskPrios example somewhat functional with task1, task2, and task4.  Task 3 still missing.  Going to update code to check if sempahore handle can be obtained in each task

--- taskPrios_chatGPT/main/main.c
+++ taskPrios_chatGPT/main/main.c
@@ -2,16 +2,17 @@
 //#include <stdlib.h>
 //#include "esp_log.h"
 //#include "sdkconfig.h"
+
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/semphr.h"
 #include "driver/gpio.h"
 
-#define TASK1_PRIORITY 4
-#define TASK2_PRIORITY 3
-#define TASK3_PRIORITY 2
-#define TASK4_PRIORITY 1
+#define TASK1_PRIORITY 1
+#define TASK2_PRIORITY 2
+#define TASK3_PRIORITY 3
+#define TASK4_PRIORITY 4
 #define USER_SWITCH_PIN 14
 
 // Task handles
@@ -73,38 +74,42 @@ void task3(void *pvParameters) {
 // Task function for Task 4
 void task4(void *pvParameters) {
     while (1) {
-        // Print message indicating Task 4 is about to take the semaphore
-        printf("\t\t\tTsk4-P4 <-\n");
+        if (semaphore != NULL) {
+            // Print message indicating Task 4 is running
+            printf("\t\t\tTsk4-P4 <-\n");
 
-        // Take the semaphore, wait indefinitely until released by ISR
-        xSemaphoreTake(semaphore, portMAX_DELAY);
+            // Task 4 functionality here
 
-        // Task 4 functionality here
+            // Run for about 10 ticks
+            for (int i = 0; i < 10; i++) {
+                // Delay for 1 tick
+                vTaskDelay(1);
+            }
 
-        // Delay for about 10 ticks
-        for (int i = 0; i < 10; i++) {
-            // Delay for 1 tick
+            // Print message indicating Task 4 is about to put itself into a blocked state
+            printf("\t\t\tTsk4-P4 ->\n");
+
+            // Block the semaphore indefinitely
+            xSemaphoreTake(semaphore, portMAX_DELAY);
+        } else {
             vTaskDelay(1);
         }
-
-        // Print message indicating Task 4 is about to block
-        printf("\t\t\tTsk4-P4 ->\n");
     }
 }
 
 // User switch interrupt service routine
 void IRAM_ATTR user_switch_isr_handler(void *arg) {
     BaseType_t xHigherPriorityTaskWoken = pdFALSE;
-    
+
     // Check the GPIO level to handle rising edge interrupt
     if (gpio_get_level(USER_SWITCH_PIN) == 1) {
         // Release the semaphore
         xSemaphoreGiveFromISR(semaphore, &xHigherPriorityTaskWoken);
     }
-    
+
     // Clear the interrupt
     // ...
-    
+
     // End the interrupt handling
     if (xHigherPriorityTaskWoken) {
         portYIELD_FROM_ISR();
@@ -112,14 +117,14 @@ void IRAM_ATTR user_switch_isr_handler(void *arg) {
 }
 
 void app_main() {
-    // Create a binary semaphore
+    // Create a binary semaphore and take it initially
     semaphore = xSemaphoreCreateBinary();
 
     // Configure user switch pin
-    //gpio_pad_select_gpio(USER_SWITCH_PIN);
     gpio_set_direction(USER_SWITCH_PIN, GPIO_MODE_INPUT);
     gpio_set_pull_mode(USER_SWITCH_PIN, GPIO_PULLDOWN_ONLY);
-    
+    gpio_set_intr_type(USER_SWITCH_PIN, GPIO_INTR_POSEDGE);
+
     // Install the user switch interrupt service routine
     gpio_install_isr_service(0);
     gpio_isr_handler_add(USER_SWITCH_PIN, user_switch_isr_handler, NULL);
@@ -131,7 +136,7 @@ void app_main() {
     xTaskCreate(task2, "Task 2", configMINIMAL_STACK_SIZE, NULL, TASK2_PRIORITY, &task2Handle);
 
     // Create Task 3
-    //xTaskCreate(task3, "Task 3", configMINIMAL_STACK_SIZE, NULL, TASK3_PRIORITY, &task3Handle);
+    xTaskCreate(task3, "Task 3", configMINIMAL_STACK_SIZE, NULL, TASK3_PRIORITY, &task3Handle);
 
     // Create Task 4
     xTaskCreate(task4, "Task 4", configMINIMAL_STACK_SIZE, NULL, TASK4_PRIORITY, &task4Handle);
